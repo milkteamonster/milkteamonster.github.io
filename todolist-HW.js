f3e2fc4fda69527fe3a1510062a19b36a993ffc9
milkteamonster <milkteamonster@gmail.com>
2023-07-16 01:43:16 +0900

    0716 update

--- todolist-HW.js
+++ todolist-HW.js
@@ -23,7 +23,7 @@ function renderData(){
         str += content
         // `<li>${item.content} <input type="button" data-num="${index}" value="刪除待辦" class="remove"></li>`
         
-    
+
     })
     const list = document.querySelector(".list");
     list.innerHTML = str;
@@ -75,6 +75,7 @@ done.addEventListener("click", function(e){
     done.setAttribute("class", "active");
     all.removeAttribute("class", "active");
     undone.removeAttribute("class", "active");
+    renderDone();
 })
 
 all.addEventListener("click", function(e){
@@ -82,6 +83,58 @@ all.addEventListener("click", function(e){
     undone.removeAttribute("class", "active");
     done.removeAttribute("class", "active");
 })
+
+
+function renderDone(){
+  let checked = document.querySelectorAll("input[type=checkbox]:checked");
+  console.log(checked) //因為是querySelectAll,回傳的是nodelist
+
+
+
+  // total=0;
+  // let str = "";
+  // checked.forEach(function(item, index){
+  //   let content = `<li>
+  //       <label class="checkbox" for="">
+  //         <input type="checkbox"/>
+  //         <span>${item.content}</span>
+  //       </label>
+  //       <a href="#" class="delete" data-num="${index}"></a>
+  //     </li>`
+  //     str += content;
+  // })
+  // const list = document.querySelector(".list");
+  // list.innerHTML = str;
+
+
+  // let total = 0;
+  // let newdone =[];
+  // let str = "";
+  // data.forEach(function(item, index){
+  //   if(item == checked){
+  //     total+=1;
+  //     newdone.push(item);
+  //   }
+  // })
+
+  // list.innerHTML = str;
+  }
+  
+  
+
+
+// let content = `<li>
+//         <label class="checkbox" for="">
+//           <input type="checkbox"/>
+//           <span>${item.content}</span>
+//         </label>
+//         <a href="#" class="delete" data-num="${index}" data-check="checked"></a>
+//       </li>`;
+
+
+
+
+
 
 //====================================================以下還沒想通先忽略
 // let content = `<li>
@@ -91,45 +144,47 @@ all.addEventListener("click", function(e){
 //         </label>
 //         <a href="#" class="delete" data-num="${index}"></a>
 //       </li>`;
-filter.addEventListener("click", function(e){
-    console.log("clicked");
-    if(e.target.value == undefined){
-        console.log("you clicked the wrong place");
-        return; //中斷了，後面其他的就不執行
-    };
-    if(e.target.value == all){
-        console.log("you clicked all");
-    };
-    let str = "";
-    data.forEach(function(item, index){
-        console.log(e.target.value == item.Charge); //button的文字跟data做比對
-        if(e.target.value == all){
-            str += `<li>
-            <label class="checkbox" for="">
-              <input type="checkbox"/>
-              <span>${item.content}</span>
-            </label>
-            <a href="#" class="delete" data-num="${index}"></a>
-          </li>`;
-        } else if(e.target.value == undone){
-            str += `<li>
-            <label class="checkbox" for="">
-              <input type="checkbox"/>
-              <span>${item.content}</span>
-            </label>
-            <a href="#" class="delete" data-num="${index}"></a>
-          </li>`;
-        } else{
-            str += `<li>
-            <label class="checkbox" for="">
-              <input type="checkbox"/>
-              <span>${item.content}</span>
-            </label>
-            <a href="#" class="delete" data-num="${index}"></a>
-          </li>`;
-        }
-    })
-    // const list = document.querySelector(".list"); 寫在外層了
-    // list.innerHTML = str;
-    renderData();
-})
+
+// filter.addEventListener("click", function(e){
+//     console.log("clicked");
+//     if(e.target.value == undefined){
+//         console.log("you clicked the wrong place");
+//         return; 
+//     };
+//     if(e.target.value == all){
+//         console.log("you clicked all");
+//     };
+//     let checkBox = document.querySelectorAll("input[type=checkbox]");
+//     console.log("checked");
+//     let str = "";
+//     data.forEach(function(item, index){
+//         console.log(e.target.value == item.Charge); 
+//         if(e.target.value == all){
+//             str += `<li>
+//             <label class="checkbox" for="">
+//               <input type="checkbox"/>
+//               <span>${item.content}</span>
+//             </label>
+//             <a href="#" class="delete" data-num="${index}"></a>
+//           </li>`;
+//         } else if(e.target.value == undone){
+//             str += `<li>
+//             <label class="checkbox" for="">
+//               <input type="checkbox"/>
+//               <span>${item.content}</span>
+//             </label>
+//             <a href="#" class="delete" data-num="${index}"></a>
+//           </li>`;
+//         } else{
+//             str += `<li>
+//             <label class="checkbox" for="">
+//               <input type="checkbox"/>
+//               <span>${item.content}</span>
+//             </label>
+//             <a href="#" class="delete" data-num="${index}"></a>
+//           </li>`;
+//         }
+//     })
+
+//     renderData();
+// })
